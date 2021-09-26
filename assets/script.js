5d66116cd2216b9173d16c21cfa2ad57e8a8b8ee
MaximeJannot <max.jannot@gmail.com>
2021-09-26 16:49:09 +0200

    0.1.0 - init project

---
+++ assets/script.js
@@ -0,0 +1,51 @@
+var requestURL = 'http://localhost/kinofil_0.1.0/assets/bdd.json';
+var request = new XMLHttpRequest();
+/* nouvelle requete */
+request.open('GET', requestURL);
+/* envoi de la requete */
+request.responseType = 'json';
+request.send();
+/* reponse du serveur et traitement */
+request.onload = function() {
+	var randomMovie = getRandomInt(request.response.movies.length);
+  var moviesRequest = request.response.movies[randomMovie];
+  cardMovie(moviesRequest);
+}
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+function cardMovie(jsonObject) {
+	var ratingSection = document.getElementById('ratingId');
+	ratingSection.classList.add(jsonObject.rating);
+
+	var quoteSection = document.getElementById('quoteId');
+	quoteSection.textContent = "« " + jsonObject.quote + " »";
+
+	var authorSection = document.getElementById('authorId');
+	authorSection.textContent = jsonObject.author + " - ";
+
+	var siteSection = document.createElement('cite');
+  siteSection.textContent = jsonObject.site;
+  authorSection.appendChild(siteSection);
+
+  var movieSection = document.getElementById('movieId');
+	movieSection.textContent = jsonObject.movie;
+	var directorSection = document.getElementById('directorId');
+	directorSection.textContent = jsonObject.director;
+	var dateSection = document.getElementById('dateId');
+	dateSection.textContent = "(" + jsonObject.publication + ")";
+
+	authorSection.setAttribute('href', jsonObject.url);
+	var blockquoteSection = document.getElementById('blockquoteId');
+	blockquoteSection.setAttribute('cite', jsonObject.url);
+}
+
+window.onload = function(){ 
+    // your code 
+	var generate = document.getElementById('generateId');
+	generate.onclick = function() {
+	  location.reload();
+	}
+};
